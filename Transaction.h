6871ef3d854e7d65f708ef131a34dd66b125786b
ADAM David Alan Martin <adamartin@FreeBSD.org>
2015-10-26 23:27:22 -0400

    Make the internals live in a "hidden" namespace.

--- Transaction.h
+++ Transaction.h
@@ -3,46 +3,49 @@
 
 namespace Alepha
 {
-	template< typename Step >
-	class TransactionStep : boost::noncopyable
+	namespace detail
 	{
-		private:
-			Step *p;
+		template< typename Step >
+		class TransactionStep : boost::noncopyable
+		{
+			private:
+				Step *p;
 
-		public:
-			explicit inline TransactionStep( Step &step ) : p( &step ) { step.activate(); }
+			public:
+				explicit inline TransactionStep( Step &step ) : p( &step ) { step.activate(); }
 
-			inline ~TransactionStep()
-			{
-				this->p->rollback();
-			}
-	};
+				inline ~TransactionStep()
+				{
+					this->p->rollback();
+				}
+		};
 
-			
-	template< typename ... Steps > class TransactionInternal;
+				
+		template< typename ... Steps > class TransactionInternal;
 
-	template< typename Step >
-	class TransactionInternal< Step > : boost::noncopyable
-	{
-		private:
-			TransactionStep< Step > s;
-			static_assert( sizeof( TransactionStep< Step > ) == sizeof( void * ), "" );
+		template< typename Step >
+		class TransactionInternal< Step > : boost::noncopyable
+		{
+			private:
+				TransactionStep< Step > s;
+				static_assert( sizeof( TransactionStep< Step > ) == sizeof( void * ), "" );
 
-		public:
-			explicit inline TransactionInternal( Step &step ) : s( step ) {}
-	};
+			public:
+				explicit inline TransactionInternal( Step &step ) : s( step ) {}
+		};
 
-	template< typename Step, typename ... Remaining >
-	class TransactionInternal< Step, Remaining...  > : boost::noncopyable
-	{
-		private:
-			TransactionStep< Step > s;
-			TransactionInternal< Remaining... > r;
+		template< typename Step, typename ... Remaining >
+		class TransactionInternal< Step, Remaining...  > : boost::noncopyable
+		{
+			private:
+				TransactionStep< Step > s;
+				TransactionInternal< Remaining... > r;
 
-		public:
-			explicit inline TransactionInternal( Step &step, Remaining ... remaining  )
-					: s( step ), r( remaining... ) {}
-	};
+			public:
+				explicit inline TransactionInternal( Step &step, Remaining ... remaining  )
+						: s( step ), r( remaining... ) {}
+		};
+	}
 
 	class Transaction : boost::noncopyable
 	{
@@ -53,7 +56,7 @@ namespace Alepha
 			Transaction( Steps ... steps )
 			{
 				std::array< std::uint8_t, sizeof...( Steps ) * sizeof( void * ) > impl_buffer;
-				new (impl_buffer.begin()) TransactionInternal< Steps... >( steps... );
+				new (impl_buffer.begin()) detail::TransactionInternal< Steps... >( steps... );
 			}
 	};
 }
